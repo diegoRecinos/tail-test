fb2c633410758f1cd0a4cbcd00d81c2a43433a20
Recinos <00182122@uca.edu.sv>
2024-12-27 08:47:31 -0600

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tailwindcss';
+
+export default defineConfig({
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
